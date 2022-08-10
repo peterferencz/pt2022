962fea0864239cbb2a402b7db0b39426890d8959
Peter <ferenczp2005@gmail.com>
2022-08-10 09:52:18 +0200

    Login window

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pt2022 {
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window {
+        public LoginWindow() {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e) {
+            if (Login()) {
+                MainWindow main = new MainWindow();
+                main.Show();
+                Close();
+            } else {
+                l_error.Content = "Invalid Credentials";
+            }
+
+        }
+
+        private bool Login() {
+            //TODO send api call
+            return false;
+        }
+    }
+}
